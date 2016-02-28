1b205a5a3773485cb721b7d829293d8b45adffe9
Istallcup <stallcui@oregonstate.edu>
2016-02-27 16:13:47 -0800

    Added model dir, Game.java file

---
+++ src/main/java/models/Game.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+* Created by IStallcup on 2/27/16.
+*/
+
+public class Game {
+	//May change, present for initial commit of Game class
+}
