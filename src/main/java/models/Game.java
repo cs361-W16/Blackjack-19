e7f08893c814a28746d667bfb13b30e158c0e4a5
Isaac Stallcup <stallcui@oregonstate.edu>
2016-03-07 13:34:49 -0800

    Added buildDeck()

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -8,6 +8,33 @@ import java.util.Random;
 * Created by IStallcup on 2/27/16.
 */
 
+
+//abstract class
 public class Game {
-	//May change, present for initial commit of Game class
+	
+	//create the deck list
+	public.java.util.List</*card type goes here*/int> deck = new ArrayList<>();
+	//use int as placeholder as the suit of the card doesn't matter in blackjack
+
+
+	//create the heand for each player?
+	public.java.util.List</*card type goes here*/int> hand = new ArrayList<>();
+
+	public int handScore = 0;
+
+	public void buildDeck()
+	{
+		for (int cNum = 2; cNum < 15; cNum++)
+		{
+			deck.add(cNum);
+			deck.add(cNum);
+			deck.add(cNum);
+			deck.add(cNum);
+		}
+	}
+
+	public Game()
+	{
+		this.buildDeck();
+	}
 }
