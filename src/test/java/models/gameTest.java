ab8552a9454f979030683b6caf81ed5931663b2d
Istallcup <stallcui@oregonstate.edu>
2016-03-10 17:53:49 -0800

    subbed assert() for assertEquals()

--- src/test/java/models/gameTest.java
+++ src/test/java/models/gameTest.java
@@ -1,11 +1,12 @@
 package models;
 
 import org.junit.Test;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 /**
  * Created by isaac on 3/9/16.
  */
+
 public class gameTest
 {
 
@@ -15,7 +16,7 @@ public class gameTest
     {
         Game g = new Game();
         //when Game is abstract, replace with instatiation of actual class
-        assert(g.deck.size() == 52);
+        assertEquals(g.deck.size(), 52);
     }
 
     @Test
@@ -24,7 +25,7 @@ public class gameTest
         Game g = new Game();
         //when Game is abstract, replace with instatiation of actual class
         /*Card*/ Integer card1 = g.getCard();
-        assert(g.deck.size() == 51);
+        assertEquals(g.deck.size(), 51);
     }
 
     @Test
@@ -33,7 +34,7 @@ public class gameTest
         Game g = new Game();
         g.getCard();
         g.getCard();
-        assert(g.hand.size() == 2);
+        assertEquals(g.hand.size(),2);
     }
 
 
