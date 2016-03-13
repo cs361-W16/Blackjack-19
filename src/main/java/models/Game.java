184d8e36d7c1e96c9a7ab719d34bc3a571f00abb
kanna_banana <kannas@oregonstate.edu>
2016-03-12 21:13:55 -0800

    game

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,58 +1,169 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-/**
-* Created by IStallcup on 2/27/16.
-*/
+import java.util.Collections;
+import java.util.ArrayList;		//game is an array of arrays
+import java.util.Random;		
 
 
-//abstract class
-public /*abstract*/ class Game {
-	
-	//create the deck list
-	public java.util.List</*card type goes here*/Integer> deck = new ArrayList<>();
-	//use int as placeholder as the suit of the card doesn't matter in blackjack
+
+//game class - has a card
+//player has a game
 
 
-	//create the hand for each player?
-	public java.util.List</*card type goes here*/Integer> hand = new ArrayList<>();
 
-	//random # generator initialization
-	Random rnd = new Random();
+public class Game implements Serializable{
 
 
-	public int handScore = 0; //does this belong here?
+	//fields
 
-	public void buildDeck()
+	public java.util.List<Card> deck;		//initialize an array of cards called Deck
+
+	public Player a_player;		
+	public Dealer a_dealer;
+
+
+
+
+	//methods
+	public Game(){
+		this.player = new player(1);
+		this.dealer = new dealer();
+		this.deck = new ArrayList<>();
+		buildDeck();
+
+
+	}
+
+
+
+	//build a deck of cards
+	public void build()
 	{
-		for (int cNum = 2; cNum < 15; cNum++)
+		for(int i =1; i<15;++i)
 		{
-			deck.add(cNum);
-			deck.add(cNum);
-			deck.add(cNum);
-			deck.add(cNum);
+			deck.add(new Card(i,Suit.Clubs));
+			deck.add(new Card(i,Suit.Hearts));
+			deck.add(new Card(i,Suit.Diamonds));
+			deck.add(new Card(i,Suit.Spades));
+
 		}
 	}
 
-	public Game()
-	{
-		this.buildDeck();
-	}
 
-	//alternative plan: write shuffle, then each time you want a card shuffle the deck, get the last card then shrink
-	// the deck array by 1. This kills the card
+			//group 13
+			public void dealer(int i)
+			{
+				if(i == 1) {
+					//Do first hand
+					if (player.getCount() == 21) {
+						player.winBlackJack(1);
+						endHand("You got Blackjack!");
+					} else {
+						//if you need to split
+						if(hasSplit && secondStand){
+							//clear your hands and shuffle
+							dealer.emptyHand(deck);
+							shuffle();
+						}
 
-	//will return the card it chooses
-	public /*Card*/ Integer getCard()
-	{
-		int removedIdx = rnd.nextInt(deck.size());
-		/*Card*/ Integer removedCard = deck.get(removedIdx);
-		deck.remove(removedIdx);
-		hand.add(removedCard);
-		return removedCard;
-	}
+
+						dealer.setHand(deal(dealer.getHand(), 2));
+						while (dealer.getCount() < 17) {
+							dealer.setHand(deal(dealer.getHand(), 1));
+						}
+						if (dealer.getCount() > 21) {
+							player.win(1);
+							endHand("Dealer busts. You win!");
+						} else if (dealer.getCount() == player.getCount()) {
+							//Start new hand with same bet
+							errorFlag = false;
+							userMessage = "Tie! New Hand";
+							againDisabled = true;
+							dealDisabled = false;
+							hitDisabled = true;
+							splitDisabled = true;
+							standDisabled = true;
+							doubleDisabled = true;
+							bettingDisabled = true;
+							previousTie = true;
+						} else if (dealer.getCount() > player.getCount()) {
+							endHand("Dealer Wins");
+						} else {
+							endHand("You win!");
+							player.win(1);
+						}
+						standDisabled = true;
+					}
+				}else if(i == 2) {
+					//Do second Hand
+					if (player.getSecondCount() == 21 && player.getSecondHand().size() == 2) {
+						player.winBlackJack(2);
+						endSecondHand("You got Blackjack!");
+					} else {
+						if(firstStand){
+							dealer.emptyHand(deck);
+							shuffle();
+						}
+						dealer.setHand(deal(dealer.getHand(), 2));
+						while (dealer.getCount() < 17) {
+							dealer.setHand(deal(dealer.getHand(), 1));
+						}
+						if (dealer.getCount() > 21) {
+							player.win(2);
+							endSecondHand("Dealer busts. You win!");
+						} else if (dealer.getCount() > player.getCount()) {
+							endSecondHand("Dealer Wins");
+						} else {
+							endSecondHand("You win!");
+							player.win(2);
+						}
+					}
+				}
+
+
+
+
+			}
+
+
+
+
+
+			//shuffle
+			public void shuffle()
+			{
+				long seed = System.nanoTime();
+				Collections.shuffle(deck,new Random(seed));
+			}
+
+
+
+
+
+			//function adds a certain number of cards to a specific hand of cards
+			public void deal (java.util.List<Card>hand,int num)
+			{
+				//do this how many every times
+				for(int i=0;i<num;++i)
+				{
+
+					hand.add(removeTop(deck));		//add to the hand by removing the top card of the deck
+
+				}
+			}
+
+
+
+			public Card removeTop(java.util.List<Card> hand)
+			{
+				return hand.remove(hand.size()-1);		//remove top card of hand
+
+			}
+
+
+
+
+
 
 
 }
