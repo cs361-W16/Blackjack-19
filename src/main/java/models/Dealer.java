715ab88d6c4fa849a2bb3cfd37a1faedfa7707c4
Istallcup <stallcui@oregonstate.edu>
2016-03-12 18:53:09 -0800

    player commit

---
+++ src/main/java/models/Dealer.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by isaac on 3/12/16.
+ */
+public class Dealer {
+}
---
+++ src/test/java/models/dealerTest.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by isaac on 3/12/16.
+ */
+public class testDealer extends Player
+{
+    @Test
+    public void testDealerInit()
+    {
+
+    }
+}
