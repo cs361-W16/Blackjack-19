49a0e3b8d6eacfe56031967adecac2a52e1feecc
Istallcup <stallcui@oregonstate.edu>
2016-03-12 18:06:14 -0800

    initial commit of player

---
+++ src/main/java/models/Player.java
@@ -0,0 +1,18 @@
+package models;
+
+/**
+ * Created by isaac on 3/12/16.
+ */
+public abstract class Player
+{
+    //in all 3:
+    //score
+    //
+
+    //user:
+    //bank
+    //bet
+    //double down
+    //ante
+
+}
---
+++ src/test/java/models/playerTest.java
@@ -0,0 +1,9 @@
+package models;
+
+/**
+ * Created by isaac on 3/12/16.
+ */
+public class playerTest
+{
+    
+}
